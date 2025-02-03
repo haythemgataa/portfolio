4c5e269118ad65e08a9e5d62abbc368523719767
Haythem Gataa <gataa.haythem@gmail.com>
2025-02-03 14:42:29 +0100

    Updated websiteURL

--- app/Profile.tsx
+++ app/Profile.tsx
@@ -20,7 +20,7 @@ const Profile: React.FC<ProfileProps> = ({
           <h1>{cv.general.displayName}</h1>
           <div className={styles.byline}>{cv.general.byline}</div>
           {cv.general.website ?
-            <a className={styles.website}>{cv.general.website}</a>
+            <a className={styles.website} href={cv.general.websiteURL} target="_blank">{cv.general.website}</a>
           : null}
         </div>
       </div>
